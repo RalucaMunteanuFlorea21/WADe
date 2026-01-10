8cff831771ca98f1fc4fa6d7801b0f62eb7fdf0c
RalucaMunteanuFlorea21 <raluca.munteanu21@yahoo.com>
2026-01-10 16:22:36 +0200

    added cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors({
+    origin: ['http://localhost:4200'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
+  const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+  await app.listen(port);
+  console.log(`API running on http://localhost:${port}`);
 }
 bootstrap();
